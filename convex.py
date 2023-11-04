cad2afa8a85cf9f572d321ff566963c85f16ba08
“Alexey <alexlomeyko2002@gmail.com>
2023-11-04 16:38:03 +0300

    In

---
+++ convex.py
@@ -0,0 +1,145 @@
+from deq import Deq
+from r2point import R2Point
+
+
+class Figure:
+    """ Абстрактная фигура """
+
+    def perimeter(self):
+        return 0.0
+
+    def area(self):
+        return 0.0
+
+    def count_sq(self):
+        return 0
+
+
+class Void(Figure):
+    """ "Hульугольник" """
+
+    def add(self, p):
+        return Point(p)
+
+    def count_sq(self):
+        return None
+
+
+class Point(Figure):
+    """ "Одноугольник" """
+
+    def __init__(self, p):
+        self.p = p
+
+    def add(self, q):
+        return self if self.p == q else Segment(self.p, q)
+
+    def count_sq(self):
+        return self.p.is_dist()
+
+
+class Segment(Figure):
+    """ "Двуугольник" """
+
+    def __init__(self, p, q):
+        self.p, self.q = p, q
+
+    def perimeter(self):
+        return 2.0 * self.p.dist(self.q)
+
+    def count_sq(self):
+        return self.p.is_dist() + self.q.is_dist()
+
+    def add(self, r):
+        if R2Point.is_triangle(self.p, self.q, r):
+            return Polygon(self.p, self.q, r)
+        elif self.q.is_inside(self.p, r):
+            return Segment(self.p, r)
+        elif self.p.is_inside(r, self.q):
+            return Segment(r, self.q)
+        else:
+            return self
+
+
+class Polygon(Figure):
+    """ Многоугольник """
+
+    def __init__(self, a, b, c):
+        self.points = Deq()
+        self.points.push_first(b)
+        if b.is_light(a, c):
+            self.points.push_first(a)
+            self.points.push_last(c)
+        else:
+            self.points.push_last(a)
+            self.points.push_first(c)
+        self._perimeter = a.dist(b) + b.dist(c) + c.dist(a)
+        self._area = abs(R2Point.area(a, b, c))
+        self._count_sq = a.is_dist() + b.is_dist() + c.is_dist()
+
+    def perimeter(self):
+        return self._perimeter
+
+    def area(self):
+        return self._area
+
+    # добавление новой точки
+    def add(self, t):
+
+        # поиск освещённого ребра
+        for n in range(self.points.size()):
+            if t.is_light(self.points.last(), self.points.first()):
+                break
+            self.points.push_last(self.points.pop_first())
+
+        # хотя бы одно освещённое ребро есть
+        if t.is_light(self.points.last(), self.points.first()):
+
+            # учёт удаления ребра, соединяющего конец и начало дека
+            self._perimeter -= self.points.first().dist(self.points.last())
+            self._area += abs(R2Point.area(t,
+                                           self.points.last(),
+                                           self.points.first()))
+
+            # удаление освещённых рёбер из начала дека
+            p = self.points.pop_first()
+            self._count_sq -= p.is_dist()
+            while t.is_light(p, self.points.first()):
+                self._perimeter -= p.dist(self.points.first())
+                self._area += abs(R2Point.area(t, p, self.points.first()))
+                p = self.points.pop_first()
+                self._count_sq -= p.is_dist()
+            self.points.push_first(p)
+            self._count_sq += p.is_dist()
+
+            # удаление освещённых рёбер из конца дека
+            p = self.points.pop_last()
+            self._count_sq -= p.is_dist()
+            while t.is_light(self.points.last(), p):
+                self._perimeter -= p.dist(self.points.last())
+                self._area += abs(R2Point.area(t, p, self.points.last()))
+                p = self.points.pop_last()
+                self._count_sq -= p.is_dist()
+            self.points.push_last(p)
+            self._count_sq += p.is_dist()
+
+            # добавление двух новых рёбер
+            self._perimeter += t.dist(self.points.first()) + \
+                t.dist(self.points.last())
+            self.points.push_first(t)
+            self._count_sq += t.is_dist()
+        return self
+
+    def count_sq(self):
+        return self._count_sq
+
+
+if __name__ == "__main__":
+    f = Void()
+    print(type(f), f.__dict__)
+    f = f.add(R2Point(0.0, 0.0))
+    print(type(f), f.__dict__)
+    f = f.add(R2Point(1.0, 0.0))
+    print(type(f), f.__dict__)
+    f = f.add(R2Point(0.0, 1.0))
+    print(type(f), f.__dict__)
---
+++ run_tk_convex.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env -S python3 -B
+from tk_drawer import TkDrawer
+from r2point import R2Point
+from convex import Void, Point, Segment, Polygon
+
+
+def void_draw(self, tk):
+    pass
+
+
+def point_draw(self, tk):
+    tk.draw_point(self.p)
+
+
+def segment_draw(self, tk):
+    tk.draw_line(self.p, self.q)
+
+
+def polygon_draw(self, tk):
+    for n in range(self.points.size()):
+        tk.draw_line(self.points.last(), self.points.first())
+        self.points.push_last(self.points.pop_first())
+
+
+setattr(Void, 'draw', void_draw)
+setattr(Point, 'draw', point_draw)
+setattr(Segment, 'draw', segment_draw)
+setattr(Polygon, 'draw', polygon_draw)
+
+
+tk = TkDrawer()
+f = Void()
+tk.clean()
+
+
+a = R2Point(0.0, 0.0)
+b = R2Point(0.0, 3.0)
+c = R2Point(3.0, 3.0)
+d = R2Point(3.0, 0.0)
+tk.draw_square(a, b, c, d)
+
+try:
+    while True:
+        tk.draw_square(a, b, c, d)
+        f = f.add(R2Point())
+        tk.clean()
+        f.draw(tk)
+        print(f"S = {f.area()}, P = {f.perimeter()}, C = {f.count_sq()} \n")
+except(EOFError, KeyboardInterrupt):
+    print("\nStop")
+    tk.close()
